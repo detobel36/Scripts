ef06a36e9240ae3b73e7e5699940ae643a8d7937
Detobel36 <detobel36@phoenix-rebirth.fr>
2017-04-04 17:05:35 +0200

    Add findWorld

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# Scripts
-Quelques petits scripts utiles
+# Script
+Voici quelques petits scripts pouvant être utile.
+
+## findWord.sh
+Permet de retrouver un mot dans un dossier.
+
+#### Utilisation
+```BASH
+./findWord.sh 
+Ce script vous permet de trouver un mot dans tous les fichiers d'un dossier
+
+Dossier/fichier à analyser [./Script]: 
+Quel mot voulez-vous trouver: test
+/home/remy/Bureau/Script/server.sh
+43.         echo "Test de connexion au serveur $i"
+/home/remy/Bureau/Script/test.sh
+39.         echo "Test de connexion au serveur $i"
+```
+
---
+++ findWord.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+echo -e "\e[37mCe script vous permet de trouver un mot dans tous les fichiers d'un dossier"
+
+echo -e "\e[97m"
+currentfile=$PWD
+read -p "Dossier/fichier à analyser [$currentfile]: " filename
+
+if [ "$filename" == "" ] ; then
+    filename=$currentfile
+fi
+
+if [[ -d $filename ]] ; then
+    read -p "Quel mot voulez-vous trouver: " word
+
+    fichier=""
+
+    grep -nri "$word" $filename/* | while read -r line ; do
+        IFS=':' read -r -a array <<< "$line"
+
+        newFichier=${array[0]}
+        if [ "$fichier" != "$newFichier" ] ; then
+            fichier=$newFichier
+            echo -e "\e[36m$fichier"
+        fi
+
+        echo -e "\e[31m${array[1]}\e[37m. \e[93m${array[2]}"
+    done
+else
+    echo "Ce dossier/fichier n'existe pas"
+fi
